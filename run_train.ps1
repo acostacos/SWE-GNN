ed88d42d64360ee86656109c63ddc46ae70d4251
acostacos <acostacarlom@gmail.com>
2025-05-11 13:12:25 +0800

    Add scripts to run init dataset

---
+++ run_train.ps1
@@ -0,0 +1,11 @@
+echo "========== initp01 =========="
+python main.py --config 'configs/initp01_config.yaml'
+
+echo "========== initp02 =========="
+python main.py --config 'configs/initp02_config.yaml'
+
+echo "========== initp03 =========="
+python main.py --config 'configs/initp03_config.yaml'
+
+echo "========== initp04 =========="
+python main.py --config 'configs/initp04_config.yaml'
